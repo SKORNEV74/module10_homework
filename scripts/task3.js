dd3fde5e0f670e82870039ee6aba23541eeca391
SKORNEV74 <yasher7495@yandex.ru>
2023-05-08 17:26:27 +0300

    task 3 (sending/receiving)

--- scripts/task3.js
+++ scripts/task3.js
@@ -4,7 +4,10 @@ const output = document.getElementById('output-block');
 const btnSend = document.querySelector('.btn-send');
 const btnGeo = document.querySelector('.btn-geo');
 
-let websocket;
+let websocket = new WebSocket(wsUrl);
+websocket.onopen = function (event) {console.log('CONNECTED');};
+websocket.onclose = function (event) {console.log('DISCONNECTED');};
+
 let input;
 
 function WriteToScreen (msg, type) {
@@ -13,7 +16,7 @@ function WriteToScreen (msg, type) {
     if (type === 'client') {
         newElem.style.alignSelf = 'flex-end';
     } else if (type === 'server') {
-        newElem.style.alignSelf = 'left';
+        newElem.style.alignSelf = 'flex-start';
     }
     newElem.style.border = '3px solid #bad7ec';
     newElem.style.padding = '7px 15px';
@@ -22,10 +25,13 @@ function WriteToScreen (msg, type) {
     output.appendChild(newElem);
 }
 btnSend.addEventListener('click', () => {
-    input = document.getElementById('input-block').value;
+    input = document.querySelector('input').value;
     console.log(input);
     btnSend.style.marginRight = '5px';
     btnGeo.style.display = 'inline-block';
     WriteToScreen(input, 'client');
     websocket.send(input);
+
+    websocket.onmessage = function (event) {WriteToScreen(event.data, 'server')};
+    websocket.onerror = function (event) {alert('ERROR:' + event.data)};
 });
