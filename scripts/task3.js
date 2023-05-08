0689e18f5184ee8779cfa2eb4bedb2a1e6b323f9
SKORNEV74 <yasher7495@yandex.ru>
2023-05-08 21:59:09 +0300

    task 3 done

--- scripts/task3.js
+++ scripts/task3.js
@@ -24,6 +24,33 @@ function WriteToScreen (msg, type) {
     newElem.innerHTML = msg;
     output.appendChild(newElem);
 }
+
+function GeolocationMsg (url) {
+    let newElem = document.createElement('a');
+    newElem.style.wordBreak = 'break-word';
+    newElem.style.alignSelf = 'flex-end';
+    newElem.style.border = '3px solid #bad7ec';
+    newElem.style.padding = '7px 15px';
+    newElem.style.margin = '0 0 5px 0';
+    newElem.innerHTML = 'Гео-локация';
+    newElem.href = url;
+    output.appendChild(newElem);
+}
+
+const error = () => {
+    alert('Невозможно получить ваше местоположение');
+}
+
+const success = (position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    let url = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    GeolocationMsg(url);
+    websocket.send(url);
+    websocket.onmessage = function (event) {console.log(event.data)};
+    websocket.onerror = function (event) {alert('ERROR:' + event.data)};
+}
+
 btnSend.addEventListener('click', () => {
     input = document.querySelector('input').value;
     console.log(input);
@@ -31,7 +58,14 @@ btnSend.addEventListener('click', () => {
     btnGeo.style.display = 'inline-block';
     WriteToScreen(input, 'client');
     websocket.send(input);
-
     websocket.onmessage = function (event) {WriteToScreen(event.data, 'server')};
     websocket.onerror = function (event) {alert('ERROR:' + event.data)};
+});
+
+btnGeo.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        alert('Geolocation не поддерживается вашим браузером');
+    } else {
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
 });
