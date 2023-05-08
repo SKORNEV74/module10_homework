58808397f0f9063a8d43072366cf8fa9ab747c08
SKORNEV74 <yasher7495@yandex.ru>
2023-05-08 12:43:17 +0300

    task 3 (in progress)

--- scripts/task3.js
+++ scripts/task3.js
@@ -1,3 +1,7 @@
 const btnSend = document.querySelector('.btn-send');
 const btnGeo = document.querySelector('.btn-geo');
 
+btnSend.addEventListener('click', () => {
+    btnSend.style.marginRight = '5px';
+    btnGeo.style.display = 'inline-block';
+});
